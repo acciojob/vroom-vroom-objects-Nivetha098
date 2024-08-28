b5e8466a50a63acc35ee6ea6be9835963309c27e
Nivetha098 <45388667+Nivetha098@users.noreply.github.com>
2024-08-28 20:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	make: "steel",
+	model:"BMW",
+	getMakeModel: function getMakeModel (getMakeModel){
+		return getMakeModel;
+	}	
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	make: "steel",
+	model:"BMW",
+	topSpeed: function topSpeed(topSpeed) {
+		return topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed());
